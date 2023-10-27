efe35193115d54563cea144f3c5f7df3078b7aab
Miguel693 <miguelag_2002@hotmail.com>
2023-10-26 23:40:46 -0300

    Añadidas importaciones de angular material

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -10,6 +10,7 @@ import { MatInputModule }     from '@angular/material/input';
 import { MatListModule }      from '@angular/material/list';
 import { MatSidenavModule }   from '@angular/material/sidenav';
 import { MatToolbarModule }   from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   exports: [
     MatButtonModule,
@@ -21,6 +22,7 @@ import { MatToolbarModule }   from '@angular/material/toolbar';
     MatListModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatDialogModule,
   ],
 })
 export class MaterialModule { }
