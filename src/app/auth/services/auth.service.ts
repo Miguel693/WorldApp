be2ac4fc0a0a5e2d644e549fe9d1995cfa829b95
Miguel693 <miguelag_2002@hotmail.com>
2023-11-03 16:33:44 -0300

    Fixed the baseUrl at checkAuthStatus

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -13,7 +13,7 @@ export class AuthService {
   private readonly baseUrl : string = environment.baseUrl;
 
   private _currentUser = signal<User | null>(null);
-  private _authStatus = signal<AuthStatus>( AuthStatus.checking);
+  private _authStatus = signal<AuthStatus>( AuthStatus.checking) ;
   private _currentRole = signal<Roles[]>( [Roles.user]);
 
   public currentUser = computed(() => this._currentUser());
@@ -24,7 +24,7 @@ export class AuthService {
     this.checkAuthStatus().subscribe();
   }
 
-  private setAuthUser( user: User, token: string): boolean{
+  private setAuthentication( user: User, token: string): boolean{
     this._currentUser.set( user );
     this._authStatus.set( AuthStatus.authenticated);
     this._currentRole.set( user.roles);
@@ -34,38 +34,35 @@ export class AuthService {
     return true;
   }
 
-  public login(email : string, password: string): Observable<boolean>{
-    const url = this.baseUrl+'/auth/login';
-    const body = {email,password};
+  login( email: string, password: string ): Observable<boolean> {
 
-    return ( this.http.post<LoginResponse>(url, body)).pipe(
-      tap( ({user, token}) => this.setAuthUser(user,token)),
-      map( () => true),
-      catchError( error => {
-        return throwError( () => error.error.message);
-      }),
-    );
+    const url  = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>( url, body )
+      .pipe(
+        map( ({ user, token }) => this.setAuthentication( user, token )),
+        catchError( err => throwError( () => err.error.message ))
+      );
   }
 
 
   public checkAuthStatus() : Observable<boolean>{
-
-    const url = `${this.baseUrl}+/auth/check-token`;
+    const url = `${this.baseUrl}/auth/check-token`;
     const token = localStorage.getItem('token');
-    const headers = new HttpHeaders().set('Authorization',`Bearer ${token}`);
-    if(!token){
+    if( !token ){
+      this.logout();
       return of(false);
-    } else {
-      return this.http.get<CheckTokenResponse>(url, {headers}).
-      pipe(
-        map( ({token,user}) => this.setAuthUser(user,token)),
-        catchError(() =>{
-          this._authStatus.set(AuthStatus.notAuthenticated);
-
-          return of(false)
-        })
-      )
     }
+    const headers = new HttpHeaders().set('Authorization',`Bearer ${token}`);
+    return this.http.get<CheckTokenResponse>(url, {headers}).
+    pipe(
+      map( ({token,user}) => this.setAuthentication(user,token)),
+      catchError(() =>{
+        this._authStatus.set(AuthStatus.notAuthenticated);
+        return of(false)
+      })
+    )
   }
 
 
@@ -73,7 +70,7 @@ export class AuthService {
     const url = this.baseUrl+'/auth/register';
     const body = { email, name, password};
     return ( this.http.post<RegisterResponse>(url, body).pipe(
-      tap( ( {user, token} ) => this.setAuthUser(user,token)),
+      tap( ( {user, token} ) => this.setAuthentication(user,token)),
       map( () => true),
       catchError( error => {
         return throwError( () => error.error.message)}),
