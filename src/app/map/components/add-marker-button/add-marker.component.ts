ade5de0c210c0219b0bf26039d83acba61e36e42
Miguel693 <miguelag_2002@hotmail.com>
2023-11-03 16:44:38 -0300

    no changes

--- src/app/map/components/add-marker-button/add-marker.component.ts
+++ src/app/map/components/add-marker-button/add-marker.component.ts
@@ -1,11 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MarkerCardComponent } from '../marker-card/marker-card.component';
-
+import { MatButton, MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'add-marker-button',
   templateUrl: './add-marker.component.html',
-  styleUrls: ['./add-marker.component.css']
+  styleUrls: ['./add-marker.component.css'],
 })
 export class AddMarkerButtonComponent {
 
