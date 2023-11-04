5fe463ce4ddb646d10bc1c3d502ec48fd4a3c7ae
Miguel693 <miguelag_2002@hotmail.com>
2023-11-04 01:38:04 -0300

    Guards added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { MapModule } from './map/map.module';
 import { isAdminGuard } from './guards/is-admin.guard';
 import { isAuthenticatedGuard } from './guards/is-authenticated.guard';
+import { isNotAuthenticatedGuard } from './guards/is-not-authenticated.guard';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule) },
+  { path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( module => module.AuthModule),
+    canActivate: [isNotAuthenticatedGuard],
+  },
   { path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( module => module.DashboardModule),
+    canActivate: [isAdminGuard],
+  },
+  { path: 'map',
+    loadChildren: () => import('./map/map.module').then( module => module.MapModule),
     canActivate: [isAuthenticatedGuard],
   },
-  { path: 'map', loadChildren: () => import('./map/map.module').then( module => module.MapModule) },
   { path: '**', redirectTo: 'auth' } ,
 ];
 
