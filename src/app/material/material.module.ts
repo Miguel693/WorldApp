162db905dcf03bd7cf32bb99aef2a953337dd2c9
Miguel693 <miguelag_2002@hotmail.com>
2023-11-05 14:24:47 -0300

    added MatProgressSpinnerModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 
 // Angular Material
-import { MatButtonModule }    from '@angular/material/button';
-import { MatCardModule }      from '@angular/material/card';
-import { MatDividerModule }   from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule }      from '@angular/material/icon';
-import { MatInputModule }     from '@angular/material/input';
-import { MatListModule }      from '@angular/material/list';
-import { MatSidenavModule }   from '@angular/material/sidenav';
-import { MatToolbarModule }   from '@angular/material/toolbar';
-import { MatDialogModule } from '@angular/material/dialog';
-
+import { MatButtonModule }          from '@angular/material/button';
+import { MatCardModule }            from '@angular/material/card';
+import { MatDialogModule }          from '@angular/material/dialog';
+import { MatDividerModule }         from '@angular/material/divider';
+import { MatFormFieldModule }       from '@angular/material/form-field';
+import { MatIconModule }            from '@angular/material/icon';
+import { MatInputModule }           from '@angular/material/input';
+import { MatListModule }            from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule }         from '@angular/material/sidenav';
+import { MatToolbarModule }         from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatDividerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
+    MatProgressSpinnerModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatDialogModule,
   ],
 })
 export class MaterialModule { }
