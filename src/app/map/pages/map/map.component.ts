8d17249e183dbf4f523b92f3c904c6d0919bb4ef
Miguel693 <miguelag_2002@hotmail.com>
2023-11-05 00:53:01 -0300

    add-marker-button now adds temporal markers

--- src/app/map/pages/map/map.component.ts
+++ src/app/map/pages/map/map.component.ts
@@ -1,5 +1,5 @@
-import { AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
-import * as mapboxgl from 'mapbox-gl';
+import { AfterViewInit, Component, ElementRef, ViewChild, Injectable } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 
 @Component({
@@ -9,18 +9,28 @@ import * as mapboxgl from 'mapbox-gl';
 
 })
 
-
+@Injectable()
 export class MapComponent implements AfterViewInit{
   @ViewChild('map') divMap ?: ElementRef;
+
+  public map ?: Map;
+  public lnglat ?: LngLat;
+  public markers ?: Marker[];
   ngAfterViewInit(): void {
     if(!this.divMap) throw 'El mapa no fue encontrado';
-    const map = new mapboxgl.Map({
-      // container: 'map',  container ID
+    this.map = new Map({
       container: this.divMap?.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-68, -38], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-68, -38],
+      zoom: 3,
     });
+
+  }
+
+  public addNewMarker(){
+    const lngLat = this.map!.getCenter();
+    const newMarker = new Marker({color : 'red', draggable: true}).setLngLat(lngLat).addTo(this.map!);
+    this.markers?.push(newMarker);
   }
 
 
